aee3ee669c4c1f0471cd4f16a1a79faec2f8d726
Emma <impanoemma@gmail.com>
2021-08-24 23:32:02 +0200

    fixing the search bar

--- src/Components/search.js
+++ src/Components/search.js
@@ -32,12 +32,12 @@ export default function Search({searchRef}){
     }
 
     return (
-        <div className="search pr-2  md:pr-0 md:block relative">
+        <div className="search mr-2  md:pr-0 md:block relative">
             <div>
                 <input 
                     ref={searchRef}
-                    className="searchbar border opacity-0 focus:opacity-10 md:block rounded-md
-                     border-gray-300 text-center text-sm md:px-8 py-1 focus:outline-none"
+                    className="searchbar border opacity-0 md:opacity-1 rounded-md
+                     border-gray-300 text-center text-sm md:px-8 p-1 focus:outline-none"
                     type="text"
                     placeholder="Search"
                     onChange={handleFilteredProfiles}
@@ -45,7 +45,7 @@ export default function Search({searchRef}){
             </div>
 
             {filteredProfiles.length > 0 && searchInput ? (
-                <div className="renderedList mt-2 absolute border border-gray-300 w-full bg-white h-56 overflow-hidden overflow-y-auto">
+                <div className="renderedList mt-2 pt-1 absolute border border-gray-300 w-full bg-white overflow-hidden overflow-y-auto">
                     <div className="arrow h-2 w-2 border-l border-t border-gray-300"></div>
                         {filteredProfiles.map(profile => (
                             <Link 
@@ -59,7 +59,7 @@ export default function Search({searchRef}){
                                     <div className="picture col-span-1 w-12 h-12">
                                         <img className="rounded-full" src={Drizzy} alt={`${profile.username}`}/>
                                     </div>
-                                    <div className="grid col-span-2 text-xs">
+                                    <div className="grid col-span-2 text-xs content-start ml-2">
                                         <span className="font-bold">{profile.username}</span>
                                         <span>{profile.fullName}</span>
                                     </div>
