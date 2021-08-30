bd0bdff70b15ccc247f61fa9e21ed75bc69286bf
Emma <impanoemma@gmail.com>
2021-08-30 20:41:13 +0200

    adding responsiveness to the single post page

--- src/Components/portal/Photo.js
+++ src/Components/portal/Photo.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Photo({photo}){
     
     return (
-        <div style={{height: "300px"}} className="singlePhoto h-60 w-full">
+        <div className="singlePhoto h-60 w-full">
             <img className="h-full object-cover" src={photo.imageSrc} alt={photo.caption}/>
         </div>
     )
--- src/Components/portal/index.js
+++ src/Components/portal/index.js
@@ -41,29 +41,58 @@ export default function Portal({photo}){
     console.log(username)
 
     return (
-        <div className="portal">
-            <div className="w-full">
-                <Header photo={photo}/>
-                <Photo photo={photo}/>
-                {userLikedPhoto !== null ? (
-                    <Actions 
+        <>
+            <div className="portal md:hidden mb-12">
+                <div className="w-full">
+                    <Header photo={photo}/>
+                    <Photo photo={photo}/>
+                    {userLikedPhoto !== null ? (
+                        <Actions 
+                            docId={photo.docId}
+                            totalLikes={photo.likes.length}
+                            likedPhoto={userLikedPhoto}
+                            handleFocus={handleFocus}
+                        />
+                    ): null}
+                    {username ? (
+                    <Footer username={username} caption={photo.caption}/>
+                    ) : <Skeleton style={{marginLeft: "1rem"}} count={1} width={300} height={20}/>}
+                    <Comments 
                         docId={photo.docId}
-                        totalLikes={photo.likes.length}
-                        likedPhoto={userLikedPhoto}
-                        handleFocus={handleFocus}
+                        comments={photo.comments}
+                        posted={photo.dateCreated}
+                        commentInput={commentInput}
                     />
-                ): null}
-                {username ? (
-                <Footer username={username} caption={photo.caption}/>
-                ) : <Skeleton style={{marginLeft: "1rem"}} count={1} width={300} height={20}/>}
-                <Comments 
-                    docId={photo.docId}
-                    comments={photo.comments}
-                    posted={photo.dateCreated}
-                    commentInput={commentInput}
-                />
-
+                </div>
             </div>
-        </div>
+
+            <div style={{minHeight: '90vh'}} className="portal hidden md:block mt-8 mx-auto">
+                <div className="w-9/12 m-auto grid grid-cols-6">
+                    <div className="col-span-4">
+                        <Photo photo={photo}/>
+                    </div>
+                    <div className="col-span-2 border ml-4">
+                        <Header photo={photo}/>
+                        {username ? (
+                        <Footer username={username} caption={photo.caption}/>
+                        ) : <Skeleton style={{marginLeft: "1rem"}} count={1} width={300} height={20}/>}
+                        {userLikedPhoto !== null ? (
+                        <Actions 
+                            docId={photo.docId}
+                            totalLikes={photo.likes.length}
+                            likedPhoto={userLikedPhoto}
+                            handleFocus={handleFocus}
+                        />
+                        ): null}
+                        <Comments 
+                            docId={photo.docId}
+                            comments={photo.comments}
+                            posted={photo.dateCreated}
+                            commentInput={commentInput}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
